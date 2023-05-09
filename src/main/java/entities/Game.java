c4e838e84c760ee00b16e205bee328f8fe4c7769
Flavio Reis <flavioreissouza@yahoo.com>
2023-05-09 20:10:55 -0300

    properties, Game, ORM

---
+++ src/main/java/entities/Game.java
@@ -0,0 +1,37 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+
+    private String genre;
+
+    private String platforms;
+
+    private Double score;
+
+    private String imgUrl;
+
+    private String shortDescription;
+
+    private String longDescription;
+}
